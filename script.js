f954b891a008707a18a2db76eac2dd04f1e991d0
Polaczeq22 <miszel803@gmail.com>
2022-04-22 01:19:47 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,276 @@
+$(function() {
+    $("#1").click(function() {
+        if($("#1").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#1").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#1").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#2").click(function() {
+        if($("#2").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#2").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#2").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#3").click(function() {
+        if($("#3").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#3").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#3").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#4").click(function() {
+        if($("#4").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#4").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#4").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#5").click(function() {
+        if($("#5").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#5").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#5").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#6").click(function() {
+        if($("#6").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#6").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#6").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#7").click(function() {
+        if($("#7").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#7").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#7").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#8").click(function() {
+        if($("#8").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#8").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#8").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#9").click(function() {
+        if($("#9").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#9").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#9").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#10").click(function() {
+        if($("#10").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#10").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#10").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#11").click(function() {
+        if($("#11").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#11").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#11").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#12").click(function() {
+        if($("#12").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#12").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#12").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#13").click(function() {
+        if($("#13").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#13").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#13").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#14").click(function() {
+        if($("#14").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#14").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#14").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#15").click(function() {
+        if($("#15").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#15").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#15").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#16").click(function() {
+        if($("#16").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#16").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#16").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#17").click(function() {
+        if($("#17").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#17").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#17").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#18").click(function() {
+        if($("#18").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#18").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#18").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#19").click(function() {
+        if($("#19").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#19").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#19").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#20").click(function() {
+        if($("#20").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#20").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#20").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#21").click(function() {
+        if($("#21").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#21").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#21").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#22").click(function() {
+        if($("#22").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#22").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#22").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#23").click(function() {
+        if($("#23").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#23").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#23").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#24").click(function() {
+        if($("#24").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#24").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#24").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+
+    $("#25").click(function() {
+        if($("#25").css("background-color") == "rgb(7, 16, 19)")
+        {
+            $("#25").css({"background-color":"#F3E6E2","transition":"background-color 0.2s ease"});
+        }
+        else
+        {
+            $("#25").css({"background-color":"#071013","transition":"background-color 0.2s ease"});
+        }
+    });
+});
